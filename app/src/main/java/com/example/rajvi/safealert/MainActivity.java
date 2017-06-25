1118c37cce1c6d8b7762a077ebae68fbbcafca32
RajviM7 <rajvi.m7@gmail.com>
2017-06-25 07:28:39 -0400

    Map Feature Added
    TODO: Eventually use maps with danger zones

--- app/src/main/java/com/example/rajvi/safealert/MainActivity.java
+++ app/src/main/java/com/example/rajvi/safealert/MainActivity.java
@@ -45,6 +45,19 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+        final MainActivity activity2 = this;
+        Button mButton6 = (Button) findViewById(R.id.button6);
+//        FloatingActionButton mbutton = (FloatingActionButton) findViewById(R.id.button);
+        mButton6.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent map = new Intent(MainActivity.this, MapsActivity.class);
+                MainActivity.this.startActivity(map);
+            }
+
+        });
     }
     
     
@@ -52,14 +65,14 @@ public class MainActivity extends AppCompatActivity {
     ActivityCompat.requestPermissions(this,
     new String[]{Manifest.permission.RECORD_AUDIO}, MIC_PERMISSION_REQUEST_CODE);
     */
-    //add the above line of code when the user hits the "I'm nervous" button. It allows app to access mic in case of necesary 911 call
-    
+    //add the above line of code when the user hits the "I'm feeling uncomfortable" button. It allows app to access mic in case of necesary 911 call
+
     /*
         public static boolean callPolice(){ //calls the police
             public static final String ACCOUNT_SID = "ACebd0e977b0959d04da953c6fed0d5131";
             public static final String AUTH_TOKEN = "c236a3a93ee47134154089aec6f875d0";
             Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-           
+
                           //  parameters( call to # aka my num to be replaced by 911, call from # aka twilio num, URI)
                           // what is URI?
              Call call = Call.creator(new PhoneNumber("+17327446890"), new PhoneNumber("+18482299702"), new URI(""http://demo.twilio.com/welcome/voice/").create();
